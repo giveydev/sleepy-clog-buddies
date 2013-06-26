3fe341f24f781065bc457c828e946979eb3454d5
Nick Reavill <nick@givey.com>
2013-06-26 16:45:44 -0700

    Basic querying with start, match, where and return sections; atrt criteria based searching

--- src/sleepy_clog_buddies/query.clj
+++ src/sleepy_clog_buddies/query.clj
@@ -6,10 +6,9 @@
   (:require [clojurewerkz.neocons.rest.cypher :as cypher]
             [sleepy-clog-buddies.shared :as scb-shared]))
 
-(defn construct-start []
+(defn construct-start [attrs]
   (let
-    [attrs sleepy-clog-buddies.query/attrs 
-     start-str
+    [start-str
       (cond
         (contains? attrs "db_id")
           (str ":" (scb-shared/nodetypes (get attrs "nodetype")) "('db_id:" (get attrs "db_id") "')")
@@ -18,22 +17,66 @@
         (contains? attrs "nodetype")
           (str ":" (scb-shared/nodetypes (get attrs "nodetype")) "('*:*')")
         :else "(*)")]
-    (println start-str)
     (str "START n=node" start-str)))
 
-(defn construct-return []
-  (str "RETURN n as info, ID(n) as id;"))
+(defn construct-match [attrs]
+  nil)
 
-(defn construct-query-string []
+; send full match string
+; e.g., {"id":71960, "match": "(n)-[:DONATESTO]->(x)<-[:DONATESTO]-(o)", "match_return" : "distinct o as info, ID(o) as id" }
+(defn construct-match-simple [attrs]
+  (cond
+    (contains? attrs "match")
+      (str "MATCH " (get attrs "match"))
+    :else nil))
+
+; this works with strings only, with a LIKE "this%" type query
+(defn where-string-sections [where-field-values]
+  (map #(str "n." (get % "field") "! =~ '(?i)" (get % "value") ".*'") where-field-values))
+
+; send criteria to search on
+; currently disabled - using construct-where-simple instead
+; currently only handles string type searches
+; e.g., {"nodetype":"users", "where": [ {"field":"first_name", "value":"n"},  {"field":"last_name", "value":"r"} ] }
+(defn construct-where [attrs]
+  (cond
+    (contains? attrs "where")
+      (let
+        [where-list (where-string-sections (get attrs "where"))]
+        (str "WHERE " (clojure.string/join " and " where-list)))
+    :else nil))
+
+; send full where string
+; e.g., {"nodetype":"users", "where": "n.first_name! =~ '(?i)n.*'" }
+(defn construct-where-simple [attrs]
+  (cond
+    (contains? attrs "where")
+      (str "WHERE " (get attrs "where"))
+    :else nil))
+
+; e.g., {"id":71960, "match": "(n)-[:DONATESTO]->(x)<-[:DONATESTO]-(o)", "match_return" : "distinct o as info, ID(o) as id" }
+(defn construct-return [attrs]
+  (cond
+    (contains? attrs "match_return")
+      (str "RETURN " (get attrs "match_return"))
+    :else (str "RETURN n as info, ID(n) as id;")))
+
+(defn construct-query-string [attrs]
   (println scb-shared/nodetypes)
-  (str (construct-start) " " (construct-return)))
+  (let
+    [query-string-components
+      (list
+        (construct-start attrs)
+        (construct-match-simple attrs)
+        (construct-where-simple attrs)
+        (construct-return attrs))]
+    (clojure.string/join " " (remove nil? query-string-components))))
 
-(defn cypher-query [attributes]
-  (def attrs attributes)
+(defn cypher-query [attrs]
   (scb-shared/neoconnect)
   (let
     [nodes 
-      (let [query-string (construct-query-string)]
+      (let [query-string (construct-query-string attrs)]
         (println query-string)
         (cypher/tquery query-string))]
     (response (map #(assoc (:data (get % "info")) "id" (get % "id")) nodes))))
--- src/sleepy_clog_buddies/shared.clj
+++ src/sleepy_clog_buddies/shared.clj
@@ -2,12 +2,12 @@
   (:use environ.core)
   (:require [clojurewerkz.neocons.rest :as neorest]))
 
-(defn neoconnect []
-  (neorest/connect! (env :neo4j-url)))
-
 ; Define allow types of node here. This is the only validation this far - any data is allowed
 (def nodetypes
   {"users"      "user"
    "charities"  "charity"
    "moments"    "moment"
    "businesses" "business"})
+
+(defn neoconnect []
+  (neorest/connect! (env :neo4j-url)))
